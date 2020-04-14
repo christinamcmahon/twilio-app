a95b7b40c3aeda22802e77f8f443b86eded2304e
christinamcmahon <christina.mcmahon1@gmail.com>
2020-04-14 13:42:05 -0700

    rails g scaffold comment ...

--- twilio-app-backend/db/schema.rb
+++ twilio-app-backend/db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_184557) do
+ActiveRecord::Schema.define(version: 2020_04_14_204128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "date_time"
+    t.string "content"
+    t.string "img"
+    t.integer "user_id"
+    t.string "type"
+    t.integer "likes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
