729186cfec8261209e75b863990042ba02b57c5b
Christina McMahon <christinamcmahon@Christinas-MacBook-Pro.local>
2020-04-16 19:31:10 -0700

    remove type from post attributes

--- twilio-app-backend/app/serializers/post_serializer.rb
+++ twilio-app-backend/app/serializers/post_serializer.rb
@@ -1,3 +1,3 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :date_time, :content, :img, :user_id, :type, :likes
+  attributes :id, :title, :date_time, :content, :img, :user_id, :likes
 end
